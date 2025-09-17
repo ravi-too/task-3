a9a1c77a33b216517f5f4c71d4f18e65d7cfd1f7
ravi-too <www.cotemate@gmail.com>
2025-09-17 16:40:50 +0400

    Initial commit

---
+++ task3(2)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PasswordCheckApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string correctPassword = "1234"; // სწორი პაროლი
+            string input;
+
+            do
+            {
+                Console.Write("Enter password: ");
+                input = Console.ReadLine();
+            }
+            while (input != correctPassword);
+
+            Console.WriteLine("Correct!");
+        }
+    }
+}
